9cd01571e876892fba594aa9337fbb9c42754303
dotrongan496 <dotrongan496@gmail.com>
2025-10-05 21:34:47 +0700

    Delete BTL_Nhom7_Arkanoid directory

--- BTL_Nhom7_Arkanoid/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.btl_nhom7_arkanoid {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.btl_nhom7_arkanoid to javafx.fxml;
-    exports org.example.btl_nhom7_arkanoid;
-}
--- BTL_Nhom7_Arkanoid/src/main/java/org/example/btl_nhom7_arkanoid/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.btl_nhom7_arkanoid;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}

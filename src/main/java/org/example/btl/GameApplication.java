21540b62e14314140575b3da5493150c4044132a
EXP-24 <duccuongntt@gmail.com>
2025-10-09 08:40:17 +0700

    Fix gameApplication

--- src/main/java/org/example/btl/GameApplication.java
+++ src/main/java/org/example/btl/GameApplication.java
@@ -4,13 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import org.example.btl.game.Brick;
+
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class GameApplication extends Application {
 
@@ -20,10 +21,20 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/org/example/btl/Game.fxml"));
+        URL fxmlUrl = Objects.requireNonNull(
+                getClass().getResource("/org/example/btl/Game.fxml"),
+                "Cannot find Game.fxml"
+        );
+        
+        Parent root = FXMLLoader.load(fxmlUrl);
         Scene scene = new Scene(root, maxWidth, maxHeight, Color.LIGHTSKYBLUE);
 
-        Image icon = new Image(getClass().getResource("/org/example/btl/images/arkanoid_512.png").toExternalForm());
+        URL iconUrl = Objects.requireNonNull(
+                getClass().getResource("/org/example/btl/images/arkanoid_512.png"),
+                "Cannot find arkanoid_512.png"
+        );
+
+        Image icon = new Image(iconUrl.toExternalForm());
         stage.getIcons().add(icon);
         stage.setTitle("Arkanoid!");
         stage.setResizable(false);

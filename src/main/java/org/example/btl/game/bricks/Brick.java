fa70283595f9aa58c6d12b52eaef17f0c7ae04e2
dotrongan496 <dotrongan496@gmail.com>
2025-10-15 01:07:38 +0700

    add map, health, takehit, damaged

--- src/main/java/org/example/btl/game/bricks/Brick.java
+++ src/main/java/org/example/btl/game/bricks/Brick.java
@@ -2,50 +2,83 @@ package org.example.btl.game;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import org.example.btl.game.GameObject;
 
 public class Brick extends GameObject {
 
     private Image brickImage;
-    private int color;
+    private int brickType;
+    private int powerUpType;
+    public  int healthBrick = 1;
 
     public Brick(double x, double y, double width, double height, int brickType) {
         super(x, y, width, height);
         String colorName = "";
+        this.brickType = brickType;
+        this.powerUpType = powerUpType;
         switch (brickType) {
             case 1:
                 colorName = "Blue";
+                this.healthBrick = 1;
                 break;
             case 2:
                 colorName = "Yellow";
+                this.healthBrick = 1;
                 break;
             case 3:
                 colorName = "Green";
+                this.healthBrick = 1;
                 break;
             case 4:
                 colorName = "Purple";
+                this.healthBrick = 1;
                 break;
             case 5:
                 colorName = "Orange";
+                this.healthBrick = 1;
+                break;
+            case 7:
+                colorName = "strong1";
+                this.healthBrick = 2;
+                break;
+            case 8:
+                colorName = "strong1";
+                this.healthBrick = 2;
+                break;
+            case 9:
+                colorName = "unbreak";
+                this.healthBrick = 999999;
                 break;
             default:
                 colorName = "Red";
+                this.healthBrick = 1;
                 break;
         }
 
 
-        String imagePath = "/org/example/btl/images/Brick_" + colorName + ".jpg";
-
-        loadImage(imagePath);
+        String imagePath = "/org/example/btl/images/Brick_" + colorName + ".png";
+        brickImage = loadImage(imagePath);
     }
 
-    private void loadImage(String imagePath) {
-        try {
-            brickImage = new Image(getClass().getResourceAsStream(imagePath));
-        } catch (Exception e) {
-            System.err.println("Không thể tải được ảnh tại đường dẫn: " + imagePath);
-            brickImage = null;
+    public void takeDamage() {
+        if (this.healthBrick > 0 && this.healthBrick < 999999) { // Gạch không thể phá vỡ sẽ không bị giảm máu
+            this.healthBrick--;
         }
     }
+
+    public boolean isDestroyed() {
+        return this.healthBrick <= 0;
+    }
+
+
+    public int getBrickType() {
+        return brickType;
+    }
+
+    public int getPowerUpType() {
+        return powerUpType;
+    }
+
 
     @Override
     public void update() {
@@ -55,7 +88,9 @@ public class Brick extends GameObject {
     public void render(GraphicsContext gc) {
         if (brickImage != null) {
             gc.drawImage(brickImage, getX(), getY(), getWidth(), getHeight());
-        } else {
+        }
+        else {
+            System.err.println("Ảnh chưa được tải.");
         }
     }
 }

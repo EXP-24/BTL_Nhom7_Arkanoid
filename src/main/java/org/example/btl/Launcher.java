2165c597de9198223eed8264300d78806f3af51d
EXP-24 <duccuongntt@gmail.com>
2025-10-14 23:49:52 +0700

    lost live

--- src/main/java/org/example/btl/Launcher.java
+++ src/main/java/org/example/btl/Launcher.java
@@ -3,6 +3,7 @@ package org.example.btl;
 import javafx.application.Application;
 
 public class Launcher {
+
     static void main(String[] args) {
         Application.launch(GameApplication.class, args);
     }

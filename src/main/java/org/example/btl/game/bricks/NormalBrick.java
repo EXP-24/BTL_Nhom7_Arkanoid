376f2fd12d374db50abc554ae31b86a0b8de1d09
dotrongan496 <dotrongan496@gmail.com>
2025-10-16 10:02:11 +0700

    main

--- src/main/java/org/example/btl/game/bricks/NormalBrick.java
+++ src/main/java/org/example/btl/game/bricks/NormalBrick.java
@@ -2,6 +2,11 @@ package org.example.btl.game.bricks;
 
 import org.example.btl.game.Brick;
 
-public class NormalBrick {
+public class NormalBrick extends Brick {
+
+    public NormalBrick(double x, double y, double width, double height, int brickType) {
+        super(x, y, width, height, brickType, brickType);
+    }
+
 
 }

84465e3c973f2570b653eef7cb4f3c492b0aa979
EXP-24 <duccuongntt@gmail.com>
2025-11-01 00:09:24 +0700

    fix lose and change image pause menu

---
+++ src/main/java/org/example/btl/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.btl;
+
+import javafx.application.Application;
+
+public class Launcher {
+    static void main(String[] args) {
+        Application.launch(GameApplication.class, args);
+    }
+}
